a84299665d8eee404ff4b3caf6c33de18615117e
willem267 <truong260703@gmail.com>
2025-02-10 22:27:39 +0700

    Configure cho entity framework core

---
+++ MyWeb_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWeb_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+            
+        }
+    }
+}
