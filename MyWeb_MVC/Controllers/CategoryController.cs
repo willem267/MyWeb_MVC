d464387fae94e0a9b9e4d83c8804c42b0175d565
willem267 <truong260703@gmail.com>
2025-02-13 22:19:10 +0700

    Add Category model and Category CRUD operations

---
+++ MyWeb_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWeb_MVC.Data;
+using MyWeb_MVC.Models;
+
+namespace MyWeb_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        // Đây là constructor, nơi dependency được inject
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; // Gán đối tượng db được inject vào biến thành viên _db
+        }
+        public IActionResult Index()
+        {
+            // Sử dụng _db để lấy danh sách các Category từ cơ sở dữ liệu
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        //create category
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //Server Validation
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Displayorder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //edit category
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? cat = _db.Categories.Find(id);
+            if(cat == null)
+            {
+                return NotFound();
+            }
+            return View(cat);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //Delete Category
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? cat = _db.Categories.Find(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            return View(cat);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj== null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MyWeb_MVC/Data/ApplicationDbContext.cs
+++ MyWeb_MVC/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyWeb_MVC.Models;
 
 namespace MyWeb_MVC.Data
 {
@@ -8,6 +9,16 @@ namespace MyWeb_MVC.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
         {
             
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Cấu hình thực thể Category và thêm dữ liệu ban đầu
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
         }
     }
 }
